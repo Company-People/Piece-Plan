6eb5e73db0d5f9ed9bb638d83bffe661df3d392b
eunoo1995 <eunoo1995@gmail.com>
2022-10-13 10:41:25 +0900

    feat: add component based files
    컴포넌트 기반 구현을 위한 파일들 추가

---
+++ public/core/Component.js
@@ -0,0 +1,21 @@
+import addEventHandlers from '../libs/eventHolder.js';
+import render from '../libs/render.js';
+
+class Component {
+  constructor(props) {
+    this.props = props;
+    this.setEvent && addEventHandlers(this.setEvent());
+  }
+
+  /**
+   * @public
+   * 컴포넌트의 state를 업데이트한다. state가 변경되면 컴포넌트는 re-rendering된다.
+   */
+  setState(newState) {
+    this.state = { ...this.state, ...newState };
+
+    render();
+  }
+}
+
+export default Component;
