e25a393d0f4dda48d7f3a791151710ac650efda7
nalsae <nalsae0207@naver.com>
2022-10-13 21:54:00 +0900

    feat: Add logics of Modals
    - 모달 관련 스크립트 작업 및 model 수정

--- models/plans.js
+++ models/plans.js
@@ -1,8 +1,13 @@
-let plans = [
+const plans = [
   {
     planId: 1,
-    date: "2022-09-22",
-    pieces: [{ pieceId: 1, title: "이두 조지기!", category: "운동", startTime: 1, endTime: 3 }],
+    date: '2022-10-11',
+    pieces: [
+      { pieceId: 1, title: '석촌호수 산책', category: 'date', startTime: 11, endTime: 13 },
+      { pieceId: 9, title: '코딩테스트 정복!', category: 'study', startTime: 14, endTime: 17 },
+    ],
     userId: 2,
   },
 ];
+
+export default plans;
---
+++ public/components/detail-modal/js/Detail.js
@@ -0,0 +1,46 @@
+import Component from '../../../core/Component.js';
+
+class Detail extends Component {
+  render() {
+    console.log(this.props);
+    const { category, title, subTitle, content, favoriteCnt, time } = this.props.targetPiece || this.props.pieces[0];
+
+    // prettier-ignore
+    return `
+      <section class="piece">
+        <h2 class="piece-title text-gradient">${title}</h2>
+        <div class="piece-info">
+          <div class="piece-label">
+            <span class="piece-time">${time}h</span>
+            <span class="piece-category">${category}</span>
+          </div>
+          <button class="piece-favorite">${favoriteCnt}</button>
+        </div>
+        <section class="piece-content">
+          <h3 class="piece-subtitle">${subTitle}</h3>
+          <p class="piece-describtion">
+            ${content}
+          </p>
+        </section>
+        <button class="btn-close"></button>
+      </section>
+      `;
+  }
+
+  closePieceDetail({ target }) {
+    if (!target.matches('.piece > .btn-close')) return;
+    document.querySelector('.modal')?.classList.remove('moving');
+  }
+
+  setEvent() {
+    return [
+      {
+        type: 'click',
+        selector: '.piece',
+        handler: this.closePieceDetail,
+      },
+    ];
+  }
+}
+
+export default Detail;
