a1f42b0405eacf2dcf4a14ffe13bb547cf1d306b
nalsae <nalsae0207@naver.com>
2022-10-18 16:11:25 +0900

    fix: Modify users data property
    - users 데이터의 프로퍼티 중 email을 id로 수정, 일부 값 수정

--- models/users.js
+++ models/users.js
@@ -1,25 +1,25 @@
 const users = [
   {
     userId: 'ef1f1539-286f-4aa0-acd7-9ddc5aa8efac',
-    id: 'queenMinseok',
+    id: 'nalsae0207',
     name: '김팀장',
-    password: '1q2w3e4r',
+    password: '00000000',
   },
   {
     userId: '1e6db1f7-3cf7-4b0a-933d-c22a164db98b',
-    email: '10rlatkdtn',
+    id: '10rlatkdtn',
     name: '김차장',
     password: '1q2w3e4r',
   },
   {
     userId: 'f3c01bd3-c491-4034-a961-bf63e988ccbf',
-    email: 'eunoo1995',
+    id: 'eunoo1995',
     name: '김과장',
     password: '1q2w3e4r',
   },
   {
     userId: '08ee5b01-5edd-48c8-ab0d-78ab5c40edb9',
-    email: 'kawaheejun',
+    id: 'kawaheejun',
     name: '전부장',
     password: '1q2w3e4r',
   },
